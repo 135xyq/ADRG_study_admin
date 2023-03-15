fd633a6b97925d635eaec080e63fe7cd9301482e
xyq <xyq135@gmail.com>
2023-03-15 09:57:14 +0800

    系统logo设置

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -14,6 +14,8 @@
 </template>
 
 <script>
+import logo from '@/assets/logo.jpg'
+
 export default {
   name: 'SidebarLogo',
   props: {
@@ -24,8 +26,8 @@ export default {
   },
   data() {
     return {
-      title: 'Vue Admin Template',
-      logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+      title: '护理知识学习系统',
+      logo
     }
   }
 }
@@ -55,7 +57,7 @@ export default {
     width: 100%;
 
     & .sidebar-logo {
-      width: 32px;
+      width: 48px;
       height: 32px;
       vertical-align: middle;
       margin-right: 12px;

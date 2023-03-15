fa597e5bab26e5535c7b4322a7b9a0d7b6b4a3de
xyq <xyq135@gmail.com>
2023-03-15 22:00:08 +0800

    学习资源 分类 新增、更新、删除

---
+++ src/views/StudySource/Category/Update.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="update-container">
+    <el-dialog :title="isEdit?'编辑':'新增'" :visible.sync="isShowDialog" width="40%">
+      <el-form :model="form">
+        <el-form-item label="分类名称" label-width="120px">
+          <el-input v-model="form.name" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="分类描述信息" label-width="120px">
+          <el-input v-model="form.description" autocomplete="off" type="textarea" />
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="onHandleCancel">取 消</el-button>
+        <el-button v-loading="loading" type="primary" @click="onHandleConfirm">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+<script>
+// 更新或新增分类
+
+import { add, update } from '@/api/studyCategory'
+
+export default {
+  name: 'Update',
+  props: {
+    isEdit: {
+      type: Boolean
+    },
+    form: {
+      type: Object
+    },
+    isShowDialog: {
+      type: Boolean
+    }
+  },
+  data() {
+    return {
+      loading: false // 提交时加载中
+    }
+  },
+  methods: {
+    /**
+     * 取消修改或编辑
+     */
+    onHandleCancel() {
+      this.$emit('endUpdate')
+    },
+    /**
+     * 确认修改或新增数据
+     * @returns {Promise<void>}
+     */
+    async onHandleConfirm() {
+      this.loading = true
+      // 修改信息
+      if (this.isEdit) {
+        const res = await update(this.form)
+        this.$message({
+          message: res.msg,
+          type: 'success'
+        })
+      } else {
+        // 新增数据
+        const res = await add(this.form)
+        this.$message({
+          message: res.msg,
+          type: 'success'
+        })
+      }
+
+      this.loading = false
+      // 结束回调，关闭弹框
+      this.$emit('endUpdate')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/StudySource/Category/index.vue
+++ src/views/StudySource/Category/index.vue
@@ -6,9 +6,9 @@
           <el-input v-model="form.name" placeholder="分类名称" />
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" icon="el-icon-search" @click="onHandleSubmit">查询</el-button>
+          <el-button icon="el-icon-search" type="primary" @click="onHandleSubmit">查询</el-button>
           <el-button icon="el-icon-refresh" @click="onHandleReset">重置</el-button>
-          <el-button type="success" icon="el-icon-plus" @click="onHandleAdd">新增分类</el-button>
+          <el-button icon="el-icon-plus" type="success" @click="onHandleAdd">新增分类</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -55,7 +55,7 @@
         >
           <template slot-scope="scope">
             <el-row class="el-row">
-              <el-button icon="el-icon-edit" size="mini" type="primary" @click="onHandleEdit(scope.row.id)">编辑分类
+              <el-button icon="el-icon-edit" size="mini" type="primary" @click="onHandleEdit(scope.row)">编辑分类
               </el-button>
               <el-button icon="el-icon-delete" size="mini" type="danger" @click="onHandleDelete(scope.row.id)">删除分类
               </el-button>
@@ -65,7 +65,7 @@
                 icon="el-icon-video-camera"
                 size="mini"
                 type="success"
-                @click="onHandleGoToVedio(scope.row.id)"
+                @click="onHandleGoToVideo(scope.row.id)"
               >视频列表
               </el-button>
               <el-button icon="el-icon-document" size="mini" type="info" @click="onHandleGoToArticle(scope.row.id)">
@@ -87,15 +87,21 @@
         @current-change="onHandleCurrentChange"
       />
     </div>
+    <Update :form="childForm" :is-edit="childIsEdit" :is-show-dialog="isShowDialog" @endUpdate="onHandleEndUpdate" />
   </div>
 </template>
 
 <script>
 // 学习视频、文章的分类
-import { page } from '@/api/studyCategory'
+import { deleteCategory, page } from '@/api/studyCategory'
+import Update from './Update'
 
 export default {
   name: 'Category',
+
+  components: {
+    Update
+  },
   data() {
     return {
       tableData: [], // 分类表格数据
@@ -105,7 +111,10 @@ export default {
         name: ''
       }, // 查询和分页数据
       total: 0, // 数据总数
-      loading: false // 是否在加载数据
+      loading: false, // 是否在加载数据
+      childIsEdit: false, // 是否是修改
+      childForm: {}, // 修改或新增的数据对象
+      isShowDialog: false // 是否展示修改表单
     }
   },
   async created() {
@@ -127,15 +136,55 @@ export default {
       // 结束数据加载loading
       this.loading = false
     },
-    onHandleEdit(id) {
-      console.log('edit', id)
+    onHandleEdit(data) {
+      this.childIsEdit = true
+      this.childForm = {
+        id: data.id,
+        name: data.name,
+        description: data.description
+      }
+      this.isShowDialog = true
     },
-    onHandleDelete(id) {
-      console.log('delete', id)
+
+    /**
+     * 删除一个分类
+     * @param id 要删除的分类
+     * @returns {Promise<void>}
+     */
+    async onHandleDelete(id) {
+      this.$confirm('此操作将删除该分类，并删除该分类下的视频和文章, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async() => {
+        const res = await deleteCategory({ id })
+        this.$message({
+          message: res.msg,
+          type: 'success'
+        })
+        // 删除后要重新获取数据
+        await this.getData()
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
+
+      await this.getData()
     },
-    onHandleGoToVedio(id) {
-      console.log('vedio', id)
+
+    /**
+     * 跳转至视频列表页面
+     * @param id 分类id
+     */
+    onHandleGoToVideo(id) {
+      console.log('video', id)
     },
+    /**
+     * 跳转至文章列表页
+     * @param id 分类id
+     */
     onHandleGoToArticle(id) {
       console.log('article', id)
     },
@@ -173,22 +222,39 @@ export default {
       }
       await this.getData()
     },
+    /**
+     * 新增一个分类
+     */
     onHandleAdd() {
-      console.log('add')
+      this.childIsEdit = false
+      this.form = {}
+      this.isShowDialog = true
+    },
+    /**
+     * 新增或更新的回调函数
+     * 关闭弹框、重置数据、重新获取数据
+     */
+    async onHandleEndUpdate() {
+      this.isShowDialog = false
+      this.childIsEdit = false
+      this.childForm = {}
+
+      await this.getData()
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.search-container{
+.search-container {
   margin: 20px 0 10px 20px;
 }
+
 .el-row {
   margin-bottom: 6px;
 }
 
-.page-container{
+.page-container {
   display: flex;
   justify-content: right;
   margin: 20px;

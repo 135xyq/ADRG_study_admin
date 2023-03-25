e7007a484d32abaf22adc4a5294560811c4c3bd5
xyq <xyq135@gmail.com>
2023-03-25 16:26:55 +0800

    题库  题目分类api

---
+++ src/api/questionCategory.js
@@ -0,0 +1,68 @@
+import request from '@/utils/request'
+
+/**
+ * 分页获取数据
+ * @param data {Object}
+ * *page:页码 *limit：每页数量 *name：名称查找 *status：状态
+ * @returns {*}
+ */
+export function page(data) {
+  return request({
+    url: '/admin/questioncategory/page',
+    method: 'post',
+    data
+  })
+}
+
+/**
+ *  删除一个分类
+ * @param data {Object}
+ * id：要删除的数据id
+ * @returns {*}
+ */
+export function deleteCategory(data) {
+  return request({
+    url: '/admin/questioncategory/delete',
+    method: 'post',
+    data
+  })
+}
+
+/**
+ * 修改分类数据
+ * @param data {Object}
+ * id：修改数据的id  *name:分类名称 *description:分类描述 *status：状态 *sort排序
+ * @returns {*}
+ */
+export function update(data) {
+  return request({
+    url: '/admin/questioncategory/update',
+    method: 'post',
+    data
+  })
+}
+
+/**
+ * 新增一个分类
+ * @param data {Object}
+ * name: 分类名称  *description：分类描述 *status：状态 *sort: 排序
+ * @returns {*}
+ */
+export function add(data) {
+  return request({
+    url: '/admin/questioncategory/add',
+    method: 'post',
+    data
+  })
+}
+
+/**
+ * 获取分类列表
+ * @returns {*}
+ */
+export function getList() {
+  return request({
+    url: '/admin/questioncategory/list',
+    method: 'post'
+  })
+}

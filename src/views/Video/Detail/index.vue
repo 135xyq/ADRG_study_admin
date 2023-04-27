ad8f71af56abb896279b89b7b2a4744981863eb0
xyq <xyq135@gmail.com>
2023-04-27 21:37:01 +0800

    学习资源 视频详情 增加点赞、评论、收藏等信息展示

--- src/views/Video/Detail/index.vue
+++ src/views/Video/Detail/index.vue
@@ -1,7 +1,23 @@
 <template>
   <div class="video-detail-container">
     <h1 class="title">{{ video.title }}</h1>
-    <div class="description">{{ video.description }}</div>
+    <div class="info">
+      <div class="item">
+        观看次数：{{ video.view_count }}
+      </div>
+      <div class="item" @click="onHandleGoToPage('StarList')">
+        <el-link :style="{fontSize:'18px'}">收藏数：{{ video.star_count }}</el-link>
+      </div>
+      <div class="item" @click="onHandleGoToPage('LikeList')">
+        <el-link :style="{fontSize:'18px'}">点赞数：{{ video.like_count }}</el-link>
+      </div>
+      <div class="item" @click="onHandleGoToPage('CommentList')">
+        <el-link :style="{fontSize:'18px'}">评论数：{{ video.comment_count }}</el-link>
+      </div>
+      <div class="item">视频时长：{{ formateDate(video.duration) }}
+      </div>
+    </div>
+    <div class="description">描述：{{ video.description }}</div>
     <video :poster="video.thumbnail_url" :src="video.url" class="video-box" controls />
   </div>
 </template>
@@ -9,6 +25,7 @@
 <script>
 // 视频详情页
 
+import { formateDate } from '@/utils/formate'
 import { videoDetail } from '@/api/video'
 
 export default {
@@ -31,24 +48,48 @@ export default {
     }
   },
   methods: {
+    formateDate,
     async getVideoDetail() {
       const res = await videoDetail({ id: this.videoId })
       this.video = res.data
+    },
+    /**
+     * 前往指定页
+     * @param pageName
+     */
+    onHandleGoToPage(pageName) {
+      this.$router.push({ name: pageName, params: { videoId: this.videoId }})
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.video-detail-container{
+.video-detail-container {
   width: 100%;
-  .title{
+
+  .title {
     text-align: center;
   }
-  .description{
+
+  .info {
+    display: flex;
+    justify-content: center;
+    font-size: 18px;
+    margin-bottom: 20px;
+    color: #606266;
+
+    .item {
+      margin: 0 10px;
+    }
+  }
+
+  .description {
     padding: 5px 20px;
+    color: #333;
   }
 }
+
 .video-box {
   margin: 20px auto;
   box-sizing: border-box;

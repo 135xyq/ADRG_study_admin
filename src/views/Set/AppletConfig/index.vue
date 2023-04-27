828cf769606c9b6b8e086e7173cb420cf5dbc3a0
xyq <xyq135@gmail.com>
2023-04-27 18:20:41 +0800

    系统配置 小程序配置 敏感词导入和优化加载

--- src/views/Set/AppletConfig/index.vue
+++ src/views/Set/AppletConfig/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <div v-loading="loading" class="applet-config-container">
+  <div v-loading="loading || importLoading" class="applet-config-container">
     <div class="down-container">
-      <el-button class="el-icon-upload2" type="primary" size="small" @click="handleDownload"> 导入敏感词</el-button>
-      <el-button class="el-icon-download" type="primary" size="small" @click="handleDownload"> 导出敏感词</el-button>
+      <el-button class="el-icon-upload2" size="small" type="success" @click="onHandleImport"> 导入敏感词</el-button>
+      <el-button class="el-icon-download" size="small" type="primary" @click="onHandleDownload"> 导出敏感词</el-button>
     </div>
     <el-form ref="form" :model="configData" label-width="210px" size="medium">
       <el-form-item label="微信小程序Appid：">
@@ -25,6 +25,7 @@
             active-text="开启自动审核"
             inactive-color="#ff4949"
             inactive-text="只能人工审核"
+            @change="onSubmit"
           />
         </el-col>
       </el-form-item>
@@ -38,6 +39,7 @@
             active-text="开启自动审核"
             inactive-color="#ff4949"
             inactive-text="只能人工审核"
+            @change="onSubmit"
           />
         </el-col>
       </el-form-item>
@@ -65,7 +67,7 @@
         </el-col>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">保存设置</el-button>
+        <el-button :loading="loading" type="primary" @click="onSubmit">保存设置</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -90,6 +92,7 @@ export default {
       loading: false,
       inputVisible: false,
       inputValue: '',
+      importLoading: false, // 导入敏感词加载
       id: '', // 配置信息的id
       isEdit: false // 是否是修改配置信息
     }
@@ -119,20 +122,25 @@ export default {
 
       this.loading = false
     },
+    /**
+     * 提交修改或新增
+     * @returns {Promise<void>}
+     */
     async onSubmit() {
+      this.loading = true
+
       // 修改配置对象
       if (this.isEdit) {
-        updateConfig({
+        const res = await updateConfig({
           ...this.configData,
           id: this.id
-        }).then(async res => {
-          this.$message({
-            message: res.msg,
-            type: 'success'
-          })
-          //  更新之后重新获取数据
-          await this.getConfigData()
         })
+        this.$message({
+          message: res.msg,
+          type: 'success'
+        })
+        //  更新之后重新获取数据
+        await this.getConfigData()
       } else {
         const res = await addConfig(this.configData)
         this.$message({
@@ -141,6 +149,8 @@ export default {
         })
         await this.getConfigData()
       }
+
+      this.loading = false
     },
     handleClose(tag) {
       this.configData.sensitive_words.splice(this.configData.sensitive_words.indexOf(tag), 1)
@@ -156,7 +166,18 @@ export default {
     handleInputConfirm() {
       const inputValue = this.inputValue
       if (inputValue) {
-        this.configData.sensitive_words.push(inputValue)
+        if (this.configData.sensitive_words.indexOf(inputValue) !== -1) {
+          this.$message({
+            message: '敏感词已存在',
+            type: 'warning',
+            duration: 1500
+          })
+        } else {
+          this.configData.sensitive_words.push(inputValue)
+        }
+
+        // 数组去重
+        // this.configData.sensitive_words = [...(new Set(this.configData.sensitive_words))]
       }
       this.inputVisible = false
       this.inputValue = ''
@@ -164,7 +185,7 @@ export default {
     /**
      * 导出敏感词
      */
-    handleDownload() {
+    onHandleDownload() {
       // 表格配置
       const options = {
         '!cols': [
@@ -177,6 +198,71 @@ export default {
       const workbook = XLSX.utils.book_new()
       XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1')
       XLSX.writeFile(workbook, '敏感词.xlsx')
+    },
+    /**
+     * 导入敏感词
+     */
+    async onHandleImport() {
+      this.importLoading = true
+
+      // 创建一个input元素来选择文件
+      const input = document.createElement('input')
+      input.type = 'file'
+      input.accept = '.xlsx'
+      input.style.display = 'none'
+
+      const that = this
+
+      // 当input元素的值发生改变时，触发上传操作
+      input.onchange = async function() {
+        const file = input.files[0] // 获取选择的文件
+        // console.log(file)
+        const reader = new FileReader() // 创建一个FileReader对象
+
+        // 读取文件
+        reader.readAsArrayBuffer(file)
+        reader.onload = function() {
+          const file = reader.result
+          const wb = XLSX.read(file, {
+            type: 'binary' // 以二进制的方式读取
+          })
+
+          const sheet0 = wb.Sheets[wb.SheetNames[0]]// sheet0代表excel表格中的第一页
+
+          // ["A1='暴力", "B1='政治"]数据格式
+          const data = XLSX.utils.sheet_to_formulae(sheet0)
+
+          // 格式化后的数据["'暴力", "'政治"]
+          const formateData = data.map(item => {
+            const itemData = item.split('=')
+            let finalData = ''
+
+            if (itemData.length > 1) {
+              finalData = itemData[1]
+
+              if (finalData.length > 0 && finalData[0] === '\'') {
+                finalData = finalData.slice(1)
+              }
+            }
+            return finalData
+          })
+
+          // console.log(formateData)
+
+          that.configData.sensitive_words.push(...formateData)
+
+          // 数组去重
+          that.configData.sensitive_words = [...(new Set(that.configData.sensitive_words))]
+
+          // 提交修改
+          that.onSubmit()
+        }
+      }
+
+      // 触发，选择文件
+      input.click()
+
+      this.importLoading = false
     }
   }
 }
@@ -186,7 +272,7 @@ export default {
 .applet-config-container {
   margin-top: 20px;
 
-  .down-container{
+  .down-container {
     margin-bottom: 30px;
     display: flex;
     justify-content: left;

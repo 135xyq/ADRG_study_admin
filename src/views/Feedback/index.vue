aa7dfce9ee0c7fa805f83e55abd0a5fa332015f8
xyq <xyq135@gmail.com>
2023-04-28 18:07:10 +0800

    用户反馈 修复表格分页查询时页码未重置bug

--- src/views/Feedback/index.vue
+++ src/views/Feedback/index.vue
@@ -199,11 +199,20 @@ export default {
   methods: {
     /**
      * 获取反馈列表
+     * @param isPage
      * @returns {Promise<void>}
      */
-    async getFeedBackData() {
+    async getFeedBackData(isPage = false) {
       this.loading = true
 
+      // 如果不是更改页码，则需要将分页数据重置
+      if (!isPage) {
+        this.searchForm = {
+          ...this.searchForm,
+          page: 1,
+          limit: 20
+        }
+      }
       const res = await getFeedbackPage(this.searchForm)
       this.total = res.data.total
       this.feedbackData = res.data.data
@@ -232,7 +241,7 @@ export default {
      */
     async onHandleSizeChange(limit) {
       this.searchForm.limit = limit
-      await this.getFeedBackData()
+      await this.getFeedBackData(true)
     },
     /**
      * 修改当前页码数
@@ -240,7 +249,7 @@ export default {
      */
     async onHandleCurrentChange(page) {
       this.searchForm.page = page
-      await this.getFeedBackData()
+      await this.getFeedBackData(true)
     },
     /**
      * 数据查询
